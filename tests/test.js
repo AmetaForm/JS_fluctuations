37b07d3a36e9207251f845a11ef7c1a4d30c3814
Aleksei <122992125+AmetaForm@users.noreply.github.com>
2024-08-08 14:25:09 +0300

    Update test.js

--- tests/test.js
+++ tests/test.js
@@ -1,2 +1,19 @@
-11231313123
-123
+(function() {
+    var intervalID;
+
+    // Функция, вызываемая при загрузке скрипта на объект
+    this.preload = function(entityID) {
+        // Установка интервала для вывода сообщения каждую секунду
+        intervalID = Script.setInterval(function() {
+            console.log("Сообщение выводится каждую секунду");
+        }, 1000); // 1000 миллисекунд = 1 секунда
+    };
+
+    // Функция, вызываемая при удалении скрипта или объекта
+    this.unload = function(entityID) {
+        // Очистка интервала при удалении скрипта
+        if (intervalID) {
+            Script.clearInterval(intervalID);
+        }
+    };
+})();
