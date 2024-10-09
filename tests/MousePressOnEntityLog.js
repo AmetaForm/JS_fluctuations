6a973b6b310ecf4e6326d8dc7355de49e6b12d6e
Aleksei <122992125+AmetaForm@users.noreply.github.com>
2024-10-09 19:19:42 +0300

    Update MousePressOnEntityLog.js

--- tests/MousePressOnEntityLog.js
+++ tests/MousePressOnEntityLog.js
@@ -1,14 +1,39 @@
-(function() {
-    // Конструктор для объекта Portal
+(function () {
     var Portal = function() {};
 
+
     Portal.prototype = {
-        // Метод, который вызывается при входе в объект
-        enterEntity: function(id) {
-            print("Entity added");
+        enterEntity: function (id) {
+            var properties = Entities.getEntityProperties(id, ["userData"]);
+            var userData;
+
+            try {
+                userData = JSON.parse(properties.userData);
+            } catch (e) {
+                console.error("Error parsing userData: ", e);
+            }
+
+            if (userData) {
+                if (userData.destination) {
+                    var destination = userData.destination;
+
+                    if (userData.destination.indexOf("bookmark:") > -1) {
+                        var bookmarkName = userData.destination.replace("bookmark:", "");
+                        destination = LocationBookmarks.getAddress(bookmarkName);
+                    }
+                    
+                    Window.location = destination;
+                } else {
+                    console.log("Please specify `destination` inside this entity's `userData`!");
+                    return;
+                }
+            } else {
+                console.log("Please specify this entity's `userData`! See README.md for instructions.");
+                return;
+            }
         }
     };
 
-    // Возвращаем экземпляр объекта Portal
+    
     return new Portal();
 });
