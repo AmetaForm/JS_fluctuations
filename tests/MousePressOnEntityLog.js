ce6d3df46e020fce4b7d428bd2f98b80dc8fc013
Aleksei <122992125+AmetaForm@users.noreply.github.com>
2024-10-09 19:22:24 +0300

    Update MousePressOnEntityLog.js

--- tests/MousePressOnEntityLog.js
+++ tests/MousePressOnEntityLog.js
@@ -4,33 +4,9 @@
 
     Portal.prototype = {
         enterEntity: function (id) {
-            var properties = Entities.getEntityProperties(id, ["userData"]);
-            var userData;
-
-            try {
-                userData = JSON.parse(properties.userData);
-            } catch (e) {
-                console.error("Error parsing userData: ", e);
-            }
-
-            if (userData) {
-                if (userData.destination) {
-                    var destination = userData.destination;
-
-                    if (userData.destination.indexOf("bookmark:") > -1) {
-                        var bookmarkName = userData.destination.replace("bookmark:", "");
-                        destination = LocationBookmarks.getAddress(bookmarkName);
-                    }
-                    
-                    Window.location = destination;
-                } else {
-                    console.log("Please specify `destination` inside this entity's `userData`!");
-                    return;
-                }
-            } else {
-                console.log("Please specify this entity's `userData`! See README.md for instructions.");
-                return;
-            }
+            print("Entity added");
+            
+            
         }
     };
 
