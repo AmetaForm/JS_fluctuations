d220b321600efb0f5953af038cb537c792d0eb70
Aleksei <122992125+AmetaForm@users.noreply.github.com>
2024-10-09 19:13:25 +0300

    Update MousePressOnEntityLog.js

--- tests/MousePressOnEntityLog.js
+++ tests/MousePressOnEntityLog.js
@@ -5,7 +5,6 @@
     Portal.prototype = {
         // Метод, который вызывается при входе в объект
         enterEntity: function(id) {
-            var entityName = Entities.getEntityProperties(id, ["name"]).name; // Получаем имя объекта
             print("Hello, World");
             }));
         }
